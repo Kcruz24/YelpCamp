09cb08f98a569cf5140a76716767117bacecc5be
Kevin O. Cruz Ojeda <Kocruz.24@gmail.com>
2021-06-10 18:59:12 -0400

    Add New incompleted

--- CampgroundCRUD/app.js
+++ CampgroundCRUD/app.js
@@ -11,13 +11,15 @@ mongoose.connect("mongodb://localhost:27017/yelp-camp", {
 });
 
 const db = mongoose.connection;
-
 db.on("error", console.error.bind(console.error, "connection error"));
 db.once("open", () => {
     console.log("Database connected");
 });
 
 const app = express();
+
+// Parse req.body
+app.use(express.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -26,17 +28,32 @@ app.get("/", (req, res) => {
     res.render("home");
 });
 
+///////////////////// INDEX /////////////////////////
 app.get("/campgrounds", async (req, res) => {
     const campgrounds = await Campground.find({});
 
     res.render("campgrounds/index", { campgrounds });
 });
 
+///////////////////// NEW /////////////////////////
+app.get("/campgrounds/new", (req, res) => {
+    res.render("campgrounds/new");
+})
+
+app.post("/campgrounds", async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+
+    res.redirect(`/campgrounds/${campground._id}`);
+})
+
+///////////////////// SHOW /////////////////////////
 app.get("/campgrounds/:id", async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     res.render("campgrounds/show", {campground});
 });
 
+///////////////////// SERVER /////////////////////////
 app.listen(port, () => {
     console.log("Serving on port", port);
 });
