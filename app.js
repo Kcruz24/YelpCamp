1505bbb504b01c73690a4de55f9c4878f99bd307
Kevin O. Cruz Ojeda <Kocruz.24@gmail.com>
2021-06-21 02:35:43 -0400

    Added images and modified the model

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
-    res.render("/campgrounds/home");
+    res.render("campgrounds/home");
 });
 
 ///////////////////// INDEX /////////////////////////
