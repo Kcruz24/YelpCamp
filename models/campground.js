e508962aa6d4ca79b82abbfb0f5b32fbdae3882c
Kevin O. Cruz Ojeda <Kocruz.24@gmail.com>
2021-06-26 14:32:21 -0400

    Campground delete middleware & changed index.ejs layout
    - This middleware deletes all the associated reviews of a deleted campground.
    
    - the index.ejs layout was changed to have 2 columns displayed instead of 1.

--- models/campground.js
+++ models/campground.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Review = require("./review");
 const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
@@ -13,6 +14,18 @@ const CampgroundSchema = new Schema({
             ref: "Review"
         }
     ]
+});
+
+// DELETE ALL ASSOCCIATED REVIEWS WITH A CAMPGROUND
+// This is a middleware
+CampgroundSchema.post("findOneAndDelete", async function (camp) {
+    if (camp) {
+        await Review.deleteMany({
+            _id: {
+                $in: camp.reviews
+            }
+        });
+    }
 });
 
 module.exports = mongoose.model("Campground", CampgroundSchema);
