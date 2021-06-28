f9caf1985a16c4dda88029393d838b791d6e3626
Kevin O. Cruz Ojeda <Kocruz.24@gmail.com>
2021-06-28 16:13:46 -0400

    Configuring Passport
    - Configured passport module
    - Created a "/fakeUser" route just to test passport hidden fields like salt and hash.

--- app.js
+++ app.js
@@ -6,10 +6,13 @@ const ejsMate = require("ejs-mate");
 const methodOverride = require("method-override");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
 
 const reviews = require("./routes/reviews");
 const campgrounds = require("./routes/campgrounds");
 const ExpressError = require("./utils/ExpressError");
+const User = require("./models/user");
 
 ///////////////// DATABASE CONNECTION //////////////////////
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
@@ -25,17 +28,19 @@ db.once("open", () => {
     console.log("Database connected");
 });
 
+/////////////////////// CONFIGS //////////////////////
 const app = express();
 
 app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-/////////////////////// CONFIGS //////////////////////
+//////////////////// MIDDLEWARES //////////////////////
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
 
+// Flash & Session //
 const sessionConfig = {
     secret: "thisshouldbeabettersecret",
     resave: false,
@@ -55,6 +60,23 @@ app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     next();
+});
+
+// Passport //
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+
+// Serialization refers to how do we store a user in a session
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.get("/fakeUser", async (req, res) => {
+    const user = new User({ email: "Kevin@gmai.com", username: "kevin" });
+    const newUser = await User.register(user, "monkey");
+
+    res.send(newUser);
 });
 
 /////////////////////// ROUTERS //////////////////////
