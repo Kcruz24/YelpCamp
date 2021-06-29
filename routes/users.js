a2b43484b4c43a49ba7f7c7a466f953d598cd4e4
Kevin O. Cruz Ojeda <Kocruz.24@gmail.com>
2021-06-29 18:12:47 -0400

    Refactoring to Users Controller
    - Refactored all users routes to a controller.
    
    - Users routes are much more readable.

--- routes/users.js
+++ routes/users.js
@@ -1,63 +1,26 @@
 const express = require("express");
 const router = express.Router();
-const catchAsyncErrors = require("../utils/catchAsyncErrors");
-const User = require("../models/user");
-const passport = require("passport");
+
+const {
+    renderRegister,
+    registerUser,
+    renderLogin,
+    authenticate,
+    loginUser,
+    logoutUser
+} = require("../controllers/users");
 
 ///////////////// REGISTER /////////////////////////
-router.get("/register", (req, res) => {
-    res.render("auth/register");
-});
+router.get("/register", renderRegister);
 
-router.post(
-    "/register",
-    catchAsyncErrors(async (req, res, next) => {
-        try {
-            const { email, username, password } = req.body;
-            const user = new User({ email, username });
-            const registeredUser = await User.register(user, password);
-            console.log(registeredUser);
-
-            req.login(registeredUser, (err) => {
-                if (err) return next(err);
-
-                req.flash("success", "Welcome to Yelp Camp!");
-                res.redirect("/campgrounds");
-            });
-        } catch (e) {
-            req.flash("error", e.message);
-            res.redirect("/register");
-        }
-    })
-);
+router.post("/register", registerUser);
 
 ///////////////// LOGIN /////////////////////////
-router.get("/login", (req, res) => {
-    res.render("auth/login");
-});
+router.get("/login", renderLogin);
 
-router.post(
-    "/login",
-    passport.authenticate("local", {
-        failureFlash: true,
-        failureRedirect: "/login"
-    }),
-    (req, res) => {
-        req.flash("success", "Welcome Back!");
-        const redirectUrl = req.session.returnTo || "/campgrounds";
-
-        delete req.session.returnTo;
-        res.redirect(redirectUrl);
-    }
-);
+router.post("/login", authenticate, loginUser);
 
 ///////////////// LOGOUT /////////////////////////
-router.get("/logout", (req, res) => {
-    // logout() comes from passport
-    req.logout();
-    req.flash("success", "Goodbye!");
-
-    res.redirect("/campgrounds");
-});
+router.get("/logout", logoutUser);
 
 module.exports = router;
